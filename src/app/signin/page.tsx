b3d558ccf21435586d2bd83a5fc1385a0ce245c5
aayush jain <aayushjain1290@gmail.com>
2024-10-26 11:08:17 +0530

    js

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,43 +5,55 @@ import Image from "next/image";
 export default function signin(){
     return(
         <>
-            <div className="relative">
-                <div className="flex justify-center items-center absolute w-screen h-screen bg-cover bg-[url()] blur-sm brightness-75">
-                    <Image src={back} className="h-full w-full" alt="Background Image" />
+            <div className="relative min-h-screen flex items-center justify-center">
+                {/* Background Image */}
+                <div className="absolute inset-0 w-full h-full bg-cover bg-center blur-sm brightness-75">
+                    <Image src={back} className="h-full w-full object-cover" alt="Background Image" />
                 </div>
 
-                <div className="absolute top-[20vh] left-[40vw] w-[30vw] h-[65vh] rounded-3xl bg-white flex flex-col">
-                    <h1 className="mx-[3.5vw] my-4 mt-[8vh] text-xl font-semibold">LOG IN</h1>
+                {/* Login Card */}
+                <div className="relative w-[90vw] sm:w-[80vw] md:w-[60vw] lg:w-[40vw] xl:w-[30vw] h-[75vh] sm:h-[70vh] md:h-[65vh] lg:h-[65vh] bg-white rounded-3xl flex flex-col items-center shadow-lg">
+                    <h1 className="mt-[6vh] text-2xl font-semibold sm:text-3xl">LOG IN</h1>
 
-                    <div className="flex items-center mb-[2vh] mx-[2.5vw] pl-4 bg-gray-200 rounded-xl">
-                        <h1 className="py-[2vh]">+91</h1>
+                    {/* Mobile Number Input */}
+                    <div className="mt-[4vh] mb-4 w-[80%] bg-gray-200 rounded-xl flex items-center px-4">
+                        <span className="text-lg">+91</span>
                         <input
                             type="text"
                             placeholder="Enter Your Mobile"
-                            className="ml-[1vw] py-[2vh] focus:border-none bg-gray-200 outline-none"
+                            className="ml-2 w-full py-3 bg-transparent outline-none"
                         />
                     </div>
 
-                    <button className="w-[25vw] mx-[2.5vw] py-3 text-xl text-white bg-black rounded-xl">GET OTP</button>
+                    {/* Get OTP Button */}
+                    <button className="w-[80%] py-3 text-xl font-medium bg-black text-white rounded-xl">
+                        GET OTP
+                    </button>
 
-                    <div className="flex justify-center items-center w-[25vw] ml-[2.5vw] mt-2 border-2 border-black rounded-xl">
+                    {/* Sign In with Email */}
+                    <div className="w-[80%] mt-4 border-2 border-black rounded-xl">
                         <a href="/login">
-                            <button className="py-3 text-xl">Sign In With Email</button>
+                            <button className="w-full py-3 text-xl">Sign In With Email</button>
                         </a>
                     </div>
 
-                    <div className="flex items-center w-[25vw] ml-[2.5vw] mt-2 border-2 border-black rounded-xl">
-                        <div className="ml-[5vw] mt-6">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" alt="Google Logo" className="w-6 h-6" />
-                        </div>
-                        <button className="py-3 text-xl mt-[2vh]">Continue using Google</button>
+                    {/* Continue with Google */}
+                    <div className="w-[80%] mt-4 flex items-center border-2 border-black rounded-xl">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
+                            alt="Google Logo"
+                            className="w-6 h-6 ml-4"
+                        />
+                        <button className="flex-grow py-3 text-xl">Continue using Google</button>
                     </div>
 
-                    <a href="/signup" className="mt-1 text-center text-black underline decoration-black">
+                    {/* Sign Up Link */}
+                    <a href="/signup" className="mt-2 text-center text-black underline decoration-black">
                         New User? Sign up
                     </a>
                 </div>
             </div>
+
 
         </>
     )
